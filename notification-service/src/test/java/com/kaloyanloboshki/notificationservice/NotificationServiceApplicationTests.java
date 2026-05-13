d27c6e5216db1799df077035f6bb256fd8d0090d
Kaloyan <kloboshki1999@gmail.com>
2026-05-13 14:31:47 +0300

    Add project structure

---
+++ notification-service/src/test/java/com/kaloyanloboshki/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaloyanloboshki.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
